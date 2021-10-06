91493df4f99554eb4a9327fa381d823223de1f51
Heegreis <run3113214@gmail.com>
2021-10-07 00:20:13 +0800

    draw bendPoints to edge

--- src/components/Edge.vue
+++ src/components/Edge.vue
@@ -1,9 +1,7 @@
 <template>
   <path ref="nodePath"
-        v-bind:d="`
-          M${edge.sections[0].startPoint.x} ${edge.sections[0].startPoint.y}
-          L${edge.sections[0].endPoint.x} ${edge.sections[0].endPoint.y}
-        `"
+        v-bind:d="pathData"
+        fill="none"
         stroke="black"
   ></path>
 </template>
@@ -17,6 +15,25 @@ export default defineComponent({
     edge: {
       type: Object,
       required: true
+    }
+  },
+  computed: {
+    pathData() {
+      const startPoint = this.edge.sections[0].startPoint
+      const startPoint_path = `M${startPoint.x},${startPoint.y}`
+
+      let bendPoints_path = ''
+      if (this.edge.sections[0].bendPoints) {
+        const bendPoints = this.edge.sections[0].bendPoints
+        bendPoints.forEach(bendPoint => {
+          bendPoints_path = bendPoints_path + ` L${bendPoint.x},${bendPoint.y}`
+        })
+      }
+
+      const endPoint = this.edge.sections[0].endPoint
+      const endPoint_path = ` L${endPoint.x},${endPoint.y}`
+
+      return startPoint_path + bendPoints_path + endPoint_path
     }
   }
 })
